35f7e357e90fff7a0a3f3dc3811cda977b2350a6
mvesparza <118832657+mvesparza@users.noreply.github.com>
2024-05-27 08:27:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_set = {"Hello World from Railway whit Flask!"}
+    my_set = {"Hello World my name is Marco"}
     return jsonify(list(my_set))
 
 
